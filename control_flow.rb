c41eb0f1d72a85dff8a3746f232a92189335ba19
Sarah Mukuti <cesarwrites24@gmail.com>
2022-08-19 15:28:54 +0300

    Done.

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,18 @@
 def admin_login(username, password)
   # your code here
+  if username == "admin" || "ADMIN" && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
+
+admin_login("admin", "12345")
+admin_login("sudo", "12345")
 
 def hows_the_weather(temperature)
   # your code here
+  
 end
 
 def fizzbuzz(num)
